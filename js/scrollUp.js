0b3a9a89fd27206deae089423ee1898f7383b2ef
Kienlabadao <cocdamagecalculator@gmail.com>
2024-08-06 16:58:40 +0700

    Quick scroll fix

--- js/scrollUp.js
+++ js/scrollUp.js
@@ -3,13 +3,19 @@ const scrollUpBtn = document.getElementById("scrollUpBtn");
 const minScroll = 1000;
 
 window.addEventListener('scroll', () => {
+    checkScroll();
+});
+
+function checkScroll() {
     if (document.documentElement.scrollTop > minScroll) {
         scrollUpBtn.classList.remove("d-none");
     } else {
         scrollUpBtn.classList.add("d-none");
     }
-});
+}
 
 function scrollUp() {
     document.documentElement.scrollTop = 0;
 }
+
+checkScroll();
